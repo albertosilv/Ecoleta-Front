9ddb1e09c6c776b46073629de0cc9765a05b04ee
Alberto Silva <alberto.silva5456@gmail.com>
2020-06-03 20:25:59 -0300

    add map

--- src/pages/CreatePoints/index.tsx
+++ src/pages/CreatePoints/index.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
 import { Link } from 'react-router-dom'
 import logo from '../../assets/logo.svg'
 import { FiArrowLeft } from 'react-icons/fi'
-const createPoints = () => {
+import { Map, TileLayer, Marker } from 'react-leaflet'
+import axios from 'axios'
+import api from '../../service/api'
+interface Item {
+    id:number,
+    title:string,
+    image_url:string
+}
+interface IBGEUFResponse{
+    sigla:string;
+}
+const CreatePoints = () => {
+    const [items, setItems] = useState<Item[]>([])
+    const [ufs,setUfs] = useState<string[]>([])
+
+    useEffect(() => {
+        api.get('items').then(res => {
+                setItems(res.data)
+        })
+    }, [])
+    useEffect(()=>{
+        axios.get<IBGEUFResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados`).then(res =>{
+            const ufInitials = res.data.map(uf => uf.sigla)
+            setUfs(ufInitials)
+        })
+
+    },[])
     return (
         <div id="page-create-point">
             <header>
@@ -39,11 +65,21 @@ const createPoints = () => {
                         <h2>Endereço</h2>
                         <span>Selecione o endereço no mapa</span>
                     </legend>
+                    <Map center={[-7.2219196, -35.9043105]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[-7.2219196, -35.9043105]} />
+                    </Map>
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor='uf'>Estado (UF)</label>
                             <select name="uf" id="uf">
-                                <option value="0"> Selecione uma UF</option>
+                                {ufs.map(uf =>{
+                                    <option value="0">{uf.sigla}</option>
+                                })}
+                                
                             </select>
                         </div>
                         <div className="field">
@@ -60,32 +96,18 @@ const createPoints = () => {
                         <span>Selecione um ou mais itens abaixo</span>
                     </legend>
                     <ul className="items-grid">
-                        <li>
-                            <img src="" alt="" />
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="" alt="" />
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="" alt="" />
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="" alt="" />
-                            <span>Óleo de cozinha</span>
-                        </li>
-                        <li>
-                            <img src="" alt="" />
-                            <span>Óleo de cozinha</span>
-                        </li>
+                        {items.map(e =>(
+                                <li key={e.id}>
+                                    <img src={e.image_url} alt={e.title} />
+                                    <span>{e.title}</span>
+                                </li>
+                        ))}
                     </ul>
                 </fieldset>
                 <button type="submit">Cadastrar ponto de coleta</button>
             </form>
-        </div>
+        </div >
     )
 }
 
-export default createPoints;
+export default CreatePoints;
