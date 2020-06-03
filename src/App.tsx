cb94bec5bff324b5248f3638ca2fb2b44ab75f39
Alberto Silva <alberto.silva5456@gmail.com>
2020-06-03 12:48:39 -0300

    add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import Header from './Header'
+import Home from './pages/Home'
 
 function App() {
   return (
-     <div>
-     </div>
+    <div>
+     <Home />
+    </div>
   );
 }
 export default App;
