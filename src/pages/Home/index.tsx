71ad27d89d7a1e52edff7c859e765d482398b1f4
Alberto Silva <alberto.silva5456@gmail.com>
2020-06-03 13:35:12 -0300

    home creation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
-
-const Home = ()=>{
+import React from 'react';
+import logo from '../../assets/logo.svg';
+import {FiLogIn} from 'react-icons/fi'
+import './styles.css'
+const Home = () => {
     return (
-        <h1> Oi</h1>
+        <div id="page-home">
+            <div className="content">
+                <header>
+                    <img src={logo} alt='Ecoleta' />
+                </header>
+                <main>
+                    <h1>Seu Markeplace de coleta de resíduos.</h1>
+                    <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente</p>
+
+                    <a href='/cadastro'>
+                        <span> <FiLogIn/> </span>
+                        <strong> Cadastre um ponto de coleta</strong>
+                    </a>
+                </main>
+
+            </div>
+        </div>
     )
 }
 
